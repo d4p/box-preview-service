9c7b90fbbd10e4a583f68c0cb36556d84462b5f3
nobody <giaro.spoj@gmail.com>
2024-03-16 20:42:02 +0100

    returning first picture

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from flask import send_file, request
 
 @app.route('/')
 @app.route('/index.html')
 def index():
     return 'Hello, World!'
+
+@app.route('/get_preview')
+def get_preview():
+    if request.args.get('type') == '1':
+        filename = 'sample_file.png'
+    else:
+        filename = '../resources/error.jpg'
+    return send_file(filename, mimetype='image/jpg')
