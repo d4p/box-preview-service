9edd56f846146e8cb1454cbfe9f8b442197b9a0d
nobody <giaro.spoj@gmail.com>
2024-03-16 22:18:56 +0100

    added working image generation with cache and based on requested image size

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,53 @@ def index():
     #openscad -o test.png --imgsize=4096,4096  --colorscheme DeepOcean box_rounded.scad
     openscad_file = os.path.join(app.root_path,'..', 'openscad', 'box_rounded.scad')
     cache_file = os.path.join(app.root_path,'..', 'cache', 'box_rounded.png')
-    status = subprocess.run(["openscad","-o",cache_file, "--imgsize=1024,1024", "--colorscheme", "DeepOcean", openscad_file], capture_output=True, text=True)
-    return 'Hello, World!' + status.stdout + status.stderr
+    return_code = subprocess.call(["openscad","-o",cache_file, "--imgsize=1024,1024", "--colorscheme", "DeepOcean", openscad_file])
+    if return_code == 0:
+        return 'Generated image'
+    else:
+        return 'Hello, World!' + str(return_code)
+    
+def parameters_validation(parameters):
+    #allow only images between 100x100 and 4096x4096
+    if int(parameters["image_size"]) > 4096 or int(parameters["image_size"]) < 100:
+        print("image_size parameter error");
+        return 0
+    
+    return 1
+
+def parameters_to_file_name(parameters):
+    #TODO: add real parameter parsing
+    file_name = 'box_rounded_' + parameters["image_size"] +'.png'
+    return file_name
+
+def create_cache_filename(file):
+    return os.path.join(app.root_path,'..', 'cache', file)
+
+def check_cache(file):
+    cache_file = create_cache_filename(file)
+    if os.path.isfile(cache_file):
+        return 1
+    else:
+        return 0
+
+def generate_preview(parameters):
+    file_name = parameters_to_file_name(parameters)
+    cache_file = create_cache_filename(file_name)
+    if (check_cache(file_name)):
+        return cache_file
+    else:
+        openscad_file = os.path.join(app.root_path,'..', 'openscad', 'box_rounded.scad')
+        return_code = subprocess.call(["openscad",
+                                       "-o",
+                                       cache_file, 
+                                       "--imgsize=" + parameters["image_size"] + "," + parameters["image_size"],
+                                       "--colorscheme", 
+                                       "DeepOcean", 
+                                       openscad_file])
+        if return_code == 0:
+            return cache_file
+        else:   
+            return 0
 
 @app.route('/get_preview')
 def get_preview():
@@ -20,4 +65,19 @@ def get_preview():
     else:
         filename = '../resources/error.jpg'
         imagetype = 'image/jpg'
+    return send_file(filename, mimetype=imagetype)
+
+@app.route('/image.png')
+def image():
+    parameters = {
+        "image_size": request.args["image_size"],
+    }
+
+    if parameters_validation(parameters) == 0:
+        filename = '../resources/error.jpg'
+        imagetype = 'image/jpg'
+    else:
+        filename = generate_preview(parameters)
+        imagetype = 'image/png'
+            
     return send_file(filename, mimetype=imagetype)
